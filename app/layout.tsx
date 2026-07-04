c2e4007175072db8fc9e5aebe72935b992f65bba
Danish Maqbool Bhat <bhatdanish1591@gmail.com>
2026-07-04 17:36:49 +0530

    Remove slogan from default tab title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
   title: {
-    default: "Photonexes Technologies | Code. Create. Innovate.",
+    default: "Photonexes Technologies",
     template: "%s | Photonexes Technologies",
   },
   description:
