cd3a2f6d29572fad4d4b68dab0d9b5e26399dda9
Danish Maqbool Bhat <bhatdanish1591@gmail.com>
2026-07-06 11:21:48 +0530

    Update favicon to use company logo instead of Vercel default

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
   metadataBase: new URL('https://photonexes.in'),
+  icons: {
+    icon: '/logo.png',
+  },
   title: {
     default: "Photonexes Technologies",
     template: "%s | Photonexes Technologies",
