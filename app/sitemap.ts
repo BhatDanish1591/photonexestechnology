08d90705198c4de204091b71dfb3089c82a0c326
Danish Maqbool Bhat <bhatdanish1591@gmail.com>
2026-07-10 10:23:05 +0530

    Remove courses from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/about',
     '/services',
     '/contact',
-    '/courses',
     '/portfolio',
     '/industries',
     '/internship',
